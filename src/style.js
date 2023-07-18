46f82085a51dd9cf5415c137b92073f128a9e830
gadpro <101449500+gadpro@users.noreply.github.com>
2023-07-18 17:21:39 +0800

    Add files via upload

---
+++ src/style.js
@@ -0,0 +1,6 @@
+function onMouseOver(){
+    document.getElementById("wxpay-qr").style.display = "block";
+};
+function onMouseOut(){
+    document.getElementById("wxpay-qr").style.display = "none";
+};
