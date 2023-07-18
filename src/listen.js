b386cbb9b4f3cc3ade61d34a6edd8e5f64d45a88
gadpro <101449500+gadpro@users.noreply.github.com>
2023-07-18 14:40:15 +0800

    Add files via upload

---
+++ src/listen.js
@@ -0,0 +1,8 @@
+document.getElementById("modeselect").addEventListener("change",function(a){
+    if(a.target.tagName == "SELECT"){
+        var userselect = document.getElementById("modeselect");
+        var index = userselect.selectedIndex;
+        if(Number(userselect.options[index].value) == 1) mode1();
+        else if(Number(userselect.options[index].value) == 2) mode2();
+    };
+});
