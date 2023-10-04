07ef2d01787dbfc856c05ac0a67e60f195bb6874
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-10-04 19:13:57 +0800

    新增一种捐赠方式

--- src/style.js
+++ src/style.js
@@ -1,6 +1,12 @@
-function onMouseOver(){
+function wx_onMouseOver(){
     document.getElementById("wxpay-qr").style.display = "block";
 };
-function onMouseOut(){
+function wx_onMouseOut(){
     document.getElementById("wxpay-qr").style.display = "none";
+};
+function zfb_onMouseOver(){
+    document.getElementById("alipay-qr").style.display = "block";
+};
+function zfb_onMouseOut(){
+    document.getElementById("alipay-qr").style.display = "none";
 };
